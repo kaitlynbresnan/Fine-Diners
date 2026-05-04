0b2eb956b9c761881787b9fec2a7da3fe402562e
EdwardLaska <laskaem@live.com>
2026-05-04 11:22:19 -0700

    update admin.py

--- src/api/admin.py
+++ src/api/admin.py
@@ -12,17 +12,6 @@ router = APIRouter(
 
 @router.post("/reset", status_code=status.HTTP_204_NO_CONTENT)
 def reset():
-    """
-    Reset the game state. Gold goes to 100, all potions are removed from
-    inventory, and all barrels are removed from inventory. Carts are all reset.
-    """
-
     with db.engine.begin() as connection:
-        connection.execute(
-            sqlalchemy.text(
-                """
-                """
-            )
-        )
-    # TODO: Implement database write logic here
-    pass
+        connection.execute(sqlalchemy.text("DELETE FROM reviews"))
+    return None
