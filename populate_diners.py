426a3bcdb56e50ec9bb514485185403720e34086
davis458 <155949554+davis458@users.noreply.github.com>
2026-05-29 19:03:16 -0700

    Fake Million Rows

--- populate_diners.py
+++ populate_diners.py
@@ -7,12 +7,7 @@ import numpy as np
 fake = Faker()
 def database_connection_url():
     dotenv.load_dotenv("default.env")
-    DB_USER: str = os.environ.get("POSTGRES_USER")
-    DB_PASSWD = os.environ.get("POSTGRES_PASSWORD")
-    DB_SERVER: str = os.environ.get("POSTGRES_SERVER")
-    DB_PORT: str = os.environ.get("POSTGRES_PORT")
-    DB_NAME: str = os.environ.get("POSTGRES_DB")
-    return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
+    return os.environ.get("POSTGRES_URI")
 
 
 # Create a new DB engine based on our connection string
